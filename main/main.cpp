b3d874c5932c92ff586417c62a8b2d19c41614d8
Pedro J. Estébanez <pedrojrulez@gmail.com>
2024-09-13 12:59:48 +0200

    Revert "WorkerThreadPool: Enhance lifetime for more flexibility"
    This reverts commit 2d1dd41ef5dcb51ddb607ba572e63b605b9191be.
    
    (cherry picked from commit e2fd88ed918a6dd67379a278105207963495941d)

--- main/main.cpp
+++ main/main.cpp
@@ -142,7 +142,6 @@ static Engine *engine = nullptr;
 static ProjectSettings *globals = nullptr;
 static Input *input = nullptr;
 static InputMap *input_map = nullptr;
-static WorkerThreadPool *worker_thread_pool = nullptr;
 static TranslationServer *translation_server = nullptr;
 static Performance *performance = nullptr;
 static PackedData *packed_data = nullptr;
@@ -689,8 +688,6 @@ Error Main::test_setup() {
 
 	register_core_settings(); // Here globals are present.
 
-	worker_thread_pool = memnew(WorkerThreadPool);
-
 	translation_server = memnew(TranslationServer);
 	tsman = memnew(TextServerManager);
 
@@ -801,8 +798,6 @@ void Main::test_cleanup() {
 	ResourceSaver::remove_custom_savers();
 	PropertyListHelper::clear_base_helpers();
 
-	WorkerThreadPool::get_singleton()->finish();
-
 #ifdef TOOLS_ENABLED
 	GDExtensionManager::get_singleton()->deinitialize_extensions(GDExtension::INITIALIZATION_LEVEL_EDITOR);
 	uninitialize_modules(MODULE_INITIALIZATION_LEVEL_EDITOR);
@@ -843,9 +838,6 @@ void Main::test_cleanup() {
 #endif // _3D_DISABLED
 	if (physics_server_2d_manager) {
 		memdelete(physics_server_2d_manager);
-	}
-	if (worker_thread_pool) {
-		memdelete(worker_thread_pool);
 	}
 	if (globals) {
 		memdelete(globals);
@@ -937,7 +929,6 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 
 	register_core_settings(); //here globals are present
 
-	worker_thread_pool = memnew(WorkerThreadPool);
 	translation_server = memnew(TranslationServer);
 	performance = memnew(Performance);
 	GDREGISTER_CLASS(Performance);
@@ -2513,10 +2504,6 @@ error:
 	}
 	if (translation_server) {
 		memdelete(translation_server);
-	}
-	if (worker_thread_pool) {
-		worker_thread_pool->finish();
-		memdelete(worker_thread_pool);
 	}
 	if (globals) {
 		memdelete(globals);
@@ -4333,8 +4320,6 @@ void Main::cleanup(bool p_force) {
 	ResourceLoader::clear_translation_remaps();
 	ResourceLoader::clear_path_remaps();
 
-	WorkerThreadPool::get_singleton()->finish();
-
 	ScriptServer::finish_languages();
 
 	// Sync pending commands that may have been queued from a different thread during ScriptServer finalization
@@ -4424,9 +4409,6 @@ void Main::cleanup(bool p_force) {
 #endif // _3D_DISABLED
 	if (physics_server_2d_manager) {
 		memdelete(physics_server_2d_manager);
-	}
-	if (worker_thread_pool) {
-		memdelete(worker_thread_pool);
 	}
 	if (globals) {
 		memdelete(globals);
