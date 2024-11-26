550d4a9ad8b4667c31e9fc8829609dfb9f2ecc40
Robert Yevdokimov <105675984+ryevdokimov@users.noreply.github.com>
2024-11-26 15:31:22 -0500

    Fix removing last digit in spinbox while update_on_text_changed is true
    (cherry picked from commit 7375fe2173c486b2ce7fe2e266659d486fddddf5)

--- scene/gui/spin_box.cpp
+++ scene/gui/spin_box.cpp
@@ -66,6 +66,10 @@ void SpinBox::_update_text(bool p_keep_line_edit) {
 }
 
 void SpinBox::_text_submitted(const String &p_string) {
+	if (p_string.is_empty()) {
+		return;
+	}
+
 	Ref<Expression> expr;
 	expr.instantiate();
 
@@ -232,8 +236,8 @@ void SpinBox::_line_edit_focus_exit() {
 	if (line_edit->is_menu_visible()) {
 		return;
 	}
-	// Discontinue because the focus_exit was caused by canceling.
-	if (Input::get_singleton()->is_action_pressed("ui_cancel")) {
+	// Discontinue because the focus_exit was caused by canceling or the text is empty.
+	if (Input::get_singleton()->is_action_pressed("ui_cancel") || line_edit->get_text().is_empty()) {
 		_update_text();
 		return;
 	}
